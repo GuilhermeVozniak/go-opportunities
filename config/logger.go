7a904cb7cfd4dd65d36f0e103f2cbf454265a2e3
Guilherme Vozniak De Almeida <90528491+GuilhermeVozniak@users.noreply.github.com>
2023-10-28 14:17:41 -0300

    feat: add custom logger

---
+++ config/logger.go
@@ -0,0 +1,74 @@
+package config
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+// ANSI color codes
+const (
+	reset  = "\033[0m"
+	red    = "\033[31m"
+	green  = "\033[32m"
+	yellow = "\033[33m"
+	blue   = "\033[34m"
+	purple = "\033[35m"
+	cyan   = "\033[36m"
+	white  = "\033[37m"
+)
+
+type Logger struct {
+	debug   *log.Logger
+	info    *log.Logger
+	warning *log.Logger
+	err     *log.Logger
+	writer  io.Writer
+}
+
+func NewLogger(prefix string) *Logger {
+	writer := io.Writer(os.Stdout)
+
+	logger := log.New(writer, prefix, log.Ldate|log.Ltime)
+	return &Logger{
+		debug:   log.New(writer, blue+">DEBUG: "+reset, logger.Flags()),
+		info:    log.New(writer, green+">INFO: "+reset, logger.Flags()),
+		warning: log.New(writer, yellow+">WARNING: "+reset, logger.Flags()),
+		err:     log.New(writer, red+">ERROR: "+reset, logger.Flags()),
+		writer:  writer,
+	}
+}
+
+// Create Non-Formatted Logs
+func (l *Logger) Debug(v ...interface{}) {
+	l.debug.Println(v...)
+}
+
+func (l *Logger) Info(v ...interface{}) {
+	l.info.Println(v...)
+}
+
+func (l *Logger) Warning(v ...interface{}) {
+	l.warning.Println(v...)
+}
+
+func (l *Logger) Error(v ...interface{}) {
+	l.err.Println(v...)
+}
+
+// Create Formatted Logs
+func (l *Logger) Debugf(format string, v ...interface{}) {
+	l.debug.Printf(format, v...)
+}
+
+func (l *Logger) Infof(format string, v ...interface{}) {
+	l.info.Printf(format, v...)
+}
+
+func (l *Logger) Warningf(format string, v ...interface{}) {
+	l.warning.Printf(format, v...)
+}
+
+func (l *Logger) Errorf(format string, v ...interface{}) {
+	l.err.Printf(format, v...)
+}
--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"github.com/GuilhermeVozniak/go-opportunities/config"
 	"github.com/GuilhermeVozniak/go-opportunities/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+
+	//initializa config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config inicialization error %v",logger)
+		return
+	}
 
 	// Initialize the routes using gin default settings
 	router.InitializeServer()
